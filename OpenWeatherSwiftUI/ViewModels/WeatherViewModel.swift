f16dfaedcc54fbdaf11aa1b177a2d9b8d9a87f22
Joshua J Varghese <joshua.varghese@curalie.com>
2023-03-21 17:16:32 +0100

    populate WeatherViewModel

--- OpenWeatherSwiftUI/ViewModels/WeatherViewModel.swift
+++ OpenWeatherSwiftUI/ViewModels/WeatherViewModel.swift
@@ -10,6 +10,44 @@ import Combine
 
 class WeatherViewModel: ObservableObject {
     
+    private var weatherService: WeatherService
     
+    @Published var weather = Weather()
+    
+    init() {
+        self.weatherService = WeatherService()
+    }
+    
+    var temperature: String {
+        if let temp = weather.temp {
+            return String(format: "%.0f", temp)
+        } else {
+            return ""
+        }
+    }
+    
+    var humidity: String {
+        if let humidity = weather.humidity {
+            return String(format: "%.0f", humidity)
+        } else {
+            return ""
+        }
+    }
+    
+    var cityName: String = ""
+    
+    func search() {
+        if let city = cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            fetchWeather(by: city)
+        }
+    }
+    
+    private func fetchWeather(by city: String) {
+        weatherService.getWeather(city: city) { weather in
+            if let weather {
+                self.weather = weather
+            }
+        }
+    }
     
 }
