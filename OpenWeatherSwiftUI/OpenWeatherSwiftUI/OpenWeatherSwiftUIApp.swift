e5e58348baac8d448475fb64b41fa60d08784273
Joshua J Varghese <joshua.varghese@curalie.com>
2023-03-21 16:28:49 +0100

    add project

---
+++ OpenWeatherSwiftUI/OpenWeatherSwiftUI/OpenWeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherSwiftUIApp.swift
+//  OpenWeatherSwiftUI
+//
+//  Created by Joshua Varghese on 21.03.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
